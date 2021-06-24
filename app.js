eb836de6c82de63e8622344d157240aa25a32f72
MechEngMalik <mecheng.malik93@gmail.com>
2021-06-24 14:06:50 +0300

    solving p2

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function sum(a, b) {
   return [sum,massege];
 }
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -30,11 +30,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
-  //eslint-disable-line
+  let n1 = a;
+  let n2 = b;
+  let multi = n1 * n2;
+  let massege = `"The product of ${n1} and ${n2} is ${multi}.`
+  return [multi,massege]
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
