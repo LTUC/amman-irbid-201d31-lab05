b1485867681ada5297de4d4488b26a5c498912dd
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-06-24 15:05:36 +0300

    solve problem4

--- app.js
+++ app.js
@@ -85,12 +85,16 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
-  //eslint-disable-line
+ let sum1=sumAndMultiply(sumArr[0],sumArr[1],sumArr[2]);
+ sum1=sum1[0]
+ 
+  return [sum1,"2,3,4 was passed in as an array of numbers, and 9 is their sum."]
+
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -106,11 +110,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
-  //eslint-disable-line
+  // let multi1 =multiply(multArr[0],multArr[1],multArr[2]);
+  // // multi1 =multi1[0];
+  // console.log(multi1);
+  // return [multi1[0],"The numbers 2,3,4 have a product of 24."];
 }
-
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+//testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
