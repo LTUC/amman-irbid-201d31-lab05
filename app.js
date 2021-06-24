e8d03190f19b23dbc7427b12d8167668a6cd2d9b
Moha-AlHanbali <Moha.AlHanbali@gmail.com>
2021-06-24 11:47:17 +0300

    solved Problem 1

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
 function sum(a, b) {
-  //eslint-disable-line
+  let number1= a;
+  let number2= b;
+  let sum= number1 + number2;
+  let return_message= `The sum of ${number1} and ${number2} is ${sum}.`
+  return [sum, return_message];
 }
+
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+
+testSum(4, 7);
+// console.log(sum(2,8));
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
