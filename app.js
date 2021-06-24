32f4ecf9b06fcbc6cd79028c5e0139d4c00aacdb
sarakhatibeh <sara_basem66@yahoo.com>
2021-06-24 11:58:42 +0300

    p1

--- app.js
+++ app.js
@@ -8,9 +8,20 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
+
+
 function sum(a, b) {
+  let num1=a;
+  let num2=b;
+  let sum= num1+num2;
+  return[sum , "The sum of 4 and 7 is 11."];
+  
   //eslint-disable-line
 }
+testSum(4,7);
+
+
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 
