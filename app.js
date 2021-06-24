fadad2985bba0e8c4e9a230072e13556e7a8d37b
samerlutfi <samerlutfi96@gmial.com>
2021-06-24 22:58:42 +0300

    q1 solved

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
+  let n1 = a;
+  let n2 = b;
+  let n3 = n1 + n2;
+  return [n3,`The sum of ${n1} and ${n2} is ${n3}.`]
   //eslint-disable-line
 }
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
