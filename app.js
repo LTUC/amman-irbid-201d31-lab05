a5841ebb6957467e0a5f98c2a63f60b8d9254e4e
abdullahnazzal <a.nazzal1995@gmail.com>
2021-06-24 15:57:15 +0300

    problem 6 solved

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function sum(a, b) {
 }
 
 // Here is the test for sum(); uncomment it to run it
-//testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -43,7 +43,7 @@ function multiply(a, b) {
 }
 
 // Here is the test for multiply(); uncomment it to run it
-//testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -73,7 +73,7 @@ function sumAndMultiply(a, b, c) {
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-//testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -102,7 +102,7 @@ function sumArray(sumArr) {
 
 // Here is the test for sumArray(); uncomment it to run it
 
-//testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -152,10 +152,37 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) {
-  //eslint-disable-line
+  //let maxLength = dynamicArray.length;
+  //let array=[];
+  let mult=1;
+  //let mult2=0;
+  //console.log(maxLength);
+  // for (let i = 0; i < dynamicArray.length; i++) {
+  //   array[i]=dynamicArray[i];
+  //   console.log(array[i]);
+  // }
+  //mult=multiply(array[0],array[1])[0];
+  //console.log(mult);
+  for (let i = 0; i < dynamicArray.length ;i++ ) {
+    mult=multiply(mult,dynamicArray[i])[0];
+    //mult2=multiply(mult,array[i])[0];
+    //console.log(mult);
+    // if(mult === 120){
+    //   break;
+    // }
+  }
+  //console.log(mult);
+  //console.log(maxLength);
+
+
+//   //let multiply1=multiply(num1,num2)[0];
+//  // let totelMultiply=multiply(multiply1,num3)[0];
+  let massage=`The numbers ${dynamicArray[0]},${dynamicArray[1]},${dynamicArray[2]},${dynamicArray[3]},${dynamicArray[4]} have a product of ${mult}.`;
+  return [mult, massage] 
+  
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
