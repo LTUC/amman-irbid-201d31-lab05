811dccef8b143ef043e40e13c7b44935c7126e33
abdullahnazzal <a.nazzal1995@gmail.com>
2021-06-24 14:37:00 +0300

    problem 3 solved

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function multiply(a, b) {
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+//testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -60,11 +60,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
+  let num1=a;
+  let num2=b;
+  let num3=c;
+  let sum=num1+num2+num3;
+  let multiply=num1*num2*num3;
+  let massage1=`${num1} and ${num2} and ${num3} sum to ${sum}.`;
+  let massage2=`The product of ${num1} and ${num2} and ${num3} is ${multiply}.`;
+  
+  return [sum, multiply, massage1, massage2]
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
